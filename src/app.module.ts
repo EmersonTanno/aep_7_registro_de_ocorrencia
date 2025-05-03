82d60ee8cc44c4bf414d995ed8a75f27640d6c78
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-03 14:59:36 -0300

    update: app.module - mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OcorrenciaModule } from './ocorrencia/ocorrencia.module';
+import 'dotenv/config'
+
+
+const mongoUrl = process.env.MONGO_URL;
+
+if (!mongoUrl) {
+  throw new Error('MONGO_URL is not defined in the environment variables.');
+}
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongoUrl),
+    OcorrenciaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
