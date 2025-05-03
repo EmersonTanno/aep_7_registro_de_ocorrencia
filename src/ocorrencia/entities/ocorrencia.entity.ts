85ad7716b7f8405ea54f1a1e115ece8a6cf78287
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-03 17:42:43 -0300

    feat: ocorrencia.entity

--- src/ocorrencia/entities/ocorrencia.entity.ts
+++ src/ocorrencia/entities/ocorrencia.entity.ts
@@ -1,1 +1,23 @@
-export class Ocorrencia {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Ocorrencia 
+{
+    @Prop({required:true})
+    nameOccurrence: string;
+
+    @Prop({required:true})
+    namePerson: string;
+
+    @Prop({required:true})
+    descricaoOccurrence: string
+
+    @Prop({required:true})
+    dateOccurrence: Date;
+
+    @Prop({required:true})
+    localOccurrence: string;
+}
+
+export type OcorrenciaDocument = Ocorrencia & Document;
+export const OcorrenciaSchema = SchemaFactory.createForClass(Ocorrencia);
