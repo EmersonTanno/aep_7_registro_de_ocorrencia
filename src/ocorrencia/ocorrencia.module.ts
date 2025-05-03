853222c2dc1438fd4af79c142622e7b4632f6eb9
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-03 17:43:41 -0300

    update: ocorrencia.module

--- src/ocorrencia/ocorrencia.module.ts
+++ src/ocorrencia/ocorrencia.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OcorrenciaService } from './ocorrencia.service';
 import { OcorrenciaController } from './ocorrencia.controller';
+import { GetAllOccurrence } from './use-case/get_all_occurrence.use_case';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ocorrencia, OcorrenciaSchema } from './entities/ocorrencia.entity';
+import { OcorrenciaRepository } from './ocorrencia.repository';
 
 @Module({
-  controllers: [OcorrenciaController],
-  providers: [OcorrenciaService],
+  imports: [
+    MongooseModule.forFeature([{name: Ocorrencia.name, schema: OcorrenciaSchema}])
+  ],
+  controllers: [
+    OcorrenciaController
+  ],
+  providers: [OcorrenciaService,
+    OcorrenciaRepository,
+    GetAllOccurrence,
+  ],
 })
 export class OcorrenciaModule {}
