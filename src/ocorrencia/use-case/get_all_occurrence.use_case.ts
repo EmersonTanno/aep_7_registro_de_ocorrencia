1c512a1ce55e632140738cd65a85dec4498f1f00
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-04 15:08:20 -0300

    update: get_all_occurrence - use DTO

--- src/ocorrencia/use-case/get_all_occurrence.use_case.ts
+++ src/ocorrencia/use-case/get_all_occurrence.use_case.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { OcorrenciaRepository } from "../ocorrencia.repository";
 import { Ocorrencia } from "../entities/ocorrencia.entity";
+import { OccurrenceResponseDto } from "../dto/ocorrencia_response.dto";
 
 @Injectable()
 export class GetAllOccurrence
@@ -9,11 +10,21 @@ export class GetAllOccurrence
         private readonly occurrenciRepository: OcorrenciaRepository,
     ) {}
 
-    async exec(): Promise<{ status: number; data: Ocorrencia[] }> {
+    async exec(): Promise<{ status: number; data: OccurrenceResponseDto[] }> {
         const occurrencies = await this.occurrenciRepository.getAllOccurrencies();
+
+        const response: OccurrenceResponseDto[] = occurrencies.map((o) => ({
+            nameOccurrence: o.nameOccurrence,
+            namePerson: o.namePerson,
+            descricaoOccurrence: o.descricaoOccurrence,
+            dateOccurrence: o.dateOccurrence.toISOString(),
+            localOccurrence: o.localOccurrence,
+        }));
+
+
         return {
             status: 200,
-            data: occurrencies,
+            data: response,
         };
     }
       
