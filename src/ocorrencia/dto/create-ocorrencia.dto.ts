caf39cbd77fccc38fdbff011794fc0a5e38d4025
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-04 15:04:51 -0300

    feat: create-ocorencia.dto

--- src/ocorrencia/dto/create-ocorrencia.dto.ts
+++ src/ocorrencia/dto/create-ocorrencia.dto.ts
@@ -1,1 +1,18 @@
-export class CreateOcorrenciaDto {}
+import { IsDateString, IsString, isString } from "class-validator"
+
+export class CreateOcorrenciaDto {
+    @IsString()
+    nameOccurrence: string;
+  
+    @IsString()
+    namePerson: string;
+  
+    @IsString()
+    descricaoOccurrence: string;
+  
+    @IsDateString()
+    dateOccurrence: string;
+  
+    @IsString()
+    localOccurrence: string;
+}
