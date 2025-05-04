36d77fe0bb3f8358ef025d38b2191005261a688a
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-04 15:05:07 -0300

    feat: ocorrencia_response.dto

---
+++ src/ocorrencia/dto/ocorrencia_response.dto.ts
@@ -0,0 +1,7 @@
+export class OccurrenceResponseDto {
+    nameOccurrence: string;
+    namePerson: string;
+    descricaoOccurrence: string;
+    dateOccurrence: string;
+    localOccurrence: string;
+}
